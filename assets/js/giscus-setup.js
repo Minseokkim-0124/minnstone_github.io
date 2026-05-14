27b2e700efb8ca73552aedfd1168ee1fc7cc4ec9
Minseokkim-0124 <76686865+Minseokkim-0124@users.noreply.github.com>
2026-05-14 09:21:34 +0000

    Deploying to gh-pages from @ Minseokkim-0124/MinseokKim-0124.github.io@715c56d2048b9ff02219931b51a0f4e3f0502c2d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Minseokkim-0124/minnstone_github.io",
+    "data-repo": "Minseokkim-0124/MinseokKim-0124.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
